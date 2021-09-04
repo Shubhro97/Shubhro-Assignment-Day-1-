600d5288e515791e6ea0708c5beab1a421ff1ef8
Shubhro97 <90031272+Shubhro97@users.noreply.github.com>
2021-09-04 06:43:37 -0700

    Shubhro Guha Biswas has completed and submitting Python Zero to Hero Assignment for Day 1.

---
+++ Shubhro Day 1 (Assignment).py
@@ -0,0 +1,24 @@
+Python 3.9.7 (tags/v3.9.7:1016ef3, Aug 30 2021, 20:19:38) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> a = 54
+>>> b = 79
+>>> a + b
+133
+>>> A = 67
+>>> B = "72"
+>>> A + B
+Traceback (most recent call last):
+  File "<pyshell#5>", line 1, in <module>
+    A + B
+TypeError: unsupported operand type(s) for +: 'int' and 'str'
+>>> Name = "Shubhro"
+>>> Marks = 85
+>>> Age = 23
+>>> print (f"The marks of {Name} is {Marks} and age is {Age}.")
+The marks of Shubhro is 85 and age is 23.
+>>> print ("Hello\t World")
+Hello	 World
+>>> print ("Hello\n World")
+Hello
+ World
+>>> 
